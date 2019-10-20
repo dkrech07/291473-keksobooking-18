72e28992e2b2cedd1e7946403d39ba2e8f76796c
Дмитрий Кречетников <dkrech07@gmail.com>
2019-10-20 23:28:26 +0500

    Вносит правки в загрузку данных с сервера

--- js/backend.js
+++ js/backend.js
@@ -30,20 +30,6 @@
 
       handleErrors(xhr, onLoad, onError);
 
-      xhr.addEventListener('load', function () {
-        if (xhr.status === STATUS_OK) {
-          onLoad(xhr.response);
-        } else {
-          onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
-        }
-      });
-      xhr.addEventListener('error', function () {
-        onError('Произошла ошибка соединения');
-      });
-      xhr.addEventListener('timeout', function () {
-        onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-      });
-
       xhr.timeout = TIMEOUT_DURATION;
 
       xhr.open('GET', URL);
